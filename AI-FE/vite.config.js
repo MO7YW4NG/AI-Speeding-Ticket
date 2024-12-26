9a73ebe74203f4df44bf44bcbc3e37db60321e40
MO7YW4NG <motywang@gmail.com>
2024-12-26 22:10:08 +0800

    Refactor Vite configuration: remove eslint plugin and add Rollup build options for external dependencies

--- AI-FE/vite.config.js
+++ AI-FE/vite.config.js
@@ -4,15 +4,27 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import viteCompression from "vite-plugin-compression";
-import eslintPlugin from "vite-plugin-eslint";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), viteCompression(), eslintPlugin()],
+  plugins: [vue(), vueDevTools(), viteCompression()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+  build: {
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
   server: {
