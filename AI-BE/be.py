5003fe835b4f500e7806456c30c128b06c7f9755
cred52499 <darren.lau.xh@gmail.com>
2025-01-09 18:37:53 +0800

    Update be.py

--- AI-BE/be.py
+++ AI-BE/be.py
@@ -14,12 +14,11 @@ def recognize_license_plate():
     with psycopg.connect(conninfo,autocommit=True) as conn:
         with conn.cursor() as cursor:
             # Use parameterized query to safely insert the name
-            sql = '''SELECT * FROM trafficviolation'''
+            sql = '''SELECT * FROM traffic_violation'''
 
             cursor.execute(sql)
 
             violations = cursor.fetchall()
-            # return ("violation", violations)
 
             for violation in violations:
                 image = violation[1] # assuming the image is in the second column
@@ -33,7 +32,7 @@ def get_unrecognized_license_plates_by_AI():
         with conn.cursor() as cursor:
     
             # Use parameterized query to safely insert the name
-            sql = '''SELECT * FROM trafficviolation WHERE recognize = 1''' # 1 means not recognized by AI
+            sql = '''SELECT * FROM traffic_violation WHERE status_code IN (11, 12);''' # 11 / 12 means not recognized by AI
             
             cursor.execute(sql)
             
@@ -41,11 +40,11 @@ def get_unrecognized_license_plates_by_AI():
             
             for entry in fetched_entries:
                 # Convert the bytea image to base64
-                image_data = entry[11]  
+                image_data = entry[10]  
                 base64_image = base64.b64encode(image_data).decode('utf-8')
 
                 entry = list(entry)
-                entry[11] = base64_image
+                entry[10] = base64_image
                 entries.append(tuple(entry))
                 
             return entries
@@ -62,72 +61,89 @@ def insert_new_violation(data):
                     photo_data = file.read()
 
                 # Use parameterized query to safely insert the name
-                sql = '''INSERT INTO trafficviolation (violation_date, violation_time, device_id, 
-                        speed_limit, vehicle_speed, license_plate, photo, 
-                        recognize, location, address, longitude, latitude) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                sql = '''INSERT INTO traffic_violation (violation_date, violation_time, device_id, 
+                        speed_limit, vehicle_speed, license_plate, licenseplate_reply_date, licenseplate_reply_time,
+                        vehicle_type, photo, status_code, district, address, longitude, latitude) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
                         '''
                 
                 cursor.execute(sql, (entry['date'], entry['time'], data['device_id'], entry['speed_limit'],
-                                entry['vehicle_speed'], entry['plate_number'], psycopg.Binary(photo_data), entry['recognize'], 
-                                entry['district'], entry['address'], entry['longitude'], entry['latitude']))
+                                entry['vehicle_speed'], entry['plate_number'], entry['reply_date'], entry['reply_time'],
+                                entry['vehicletype'], psycopg.Binary(photo_data), entry['status'], entry['district'], 
+                                entry['address'], entry['longitude'], entry['latitude']))
                 
-        print("New violation(s) added successfully.")
+        return ("New violation(s) added successfully.")
         
-@router.post("/violation/update")
+@router.post("/violation/update_license_plate")
 def artificial_recognize_license_plate(violation_id, new_license_plate):
     with psycopg.connect(conninfo, autocommit=True) as conn:
         with conn.cursor() as cursor:
             
             sql =   '''  
-                    UPDATE trafficviolation 
-                    SET license_plate = %s, recognize = 0 
+                    UPDATE traffic_violation 
+                    SET license_plate = %s, status_code = 0 
                     WHERE violation_id = %s;
                     '''
             
             cursor.execute(sql, (new_license_plate, violation_id))
         
-        print("License plate updated successfully.")
+        return ("License plate updated successfully.")
 
-# articial_recognize_license_plate(1, "ABC1234")
     
-@router.post("/violation/remove")
-def move_and_delete_unrecognized_license_plate(violation_id):
+# @router.post("/violation/remove")
+# def move_and_delete_unrecognized_license_plate(violation_id):
+#     with psycopg.connect(conninfo,autocommit=True) as conn:
+#         with conn.cursor() as cursor:
+
+#             # Select the entry to be moved
+#             select_sql =    '''
+#                             SELECT violation_id, violation_date, violation_time, device_id, 
+#                             speed_limit, vehicle_speed, license_plate, licenseplatereplydate, 
+#                             licenseplatereplytime, vehicletype, vehiclestatuscode, photo, 
+#                             recognize, license_plate2, location, address, longitude, latitude
+#                             FROM trafficviolation WHERE violation_id = %s
+#                             '''
+#             cursor.execute(select_sql, (violation_id,))
+#             violation = cursor.fetchone()
+#             cursor.execute(select_sql, (violation_id,))
+#             violation = cursor.fetchone()
+
+#             if violation:
+#                 # Insert the entry into the abandoned table
+#                 insert_sql =    '''
+#                                 INSERT INTO abandoned (violation_id, violation_date, violation_time, device_id, 
+#                                 speed_limit, vehicle_speed, license_plate, licenseplatereplydate, 
+#                                 licenseplatereplytime, vehicletype, vehiclestatuscode, photo, 
+#                                 recognize, license_plate2, location, address, longitude, latitude)
+#                                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+#                                 '''
+                
+#                 cursor.execute(insert_sql, violation) 
+
+#                 # Delete the entry from the original table
+#                 delete_sql = '''DELETE FROM trafficviolation WHERE violation_id = %s'''
+#                 cursor.execute(delete_sql, (violation_id,))
+
+#                 print("License plate moved and deleted successfully.")
+#             else:
+#                 print("Violation ID not found.")
+
+@router.post("/violation/update_status")
+def update_traffic_violation(violation_id, status_code):
     with psycopg.connect(conninfo,autocommit=True) as conn:
         with conn.cursor() as cursor:
 
-            # Select the entry to be moved
-            select_sql =    '''
-                            SELECT violation_id, violation_date, violation_time, device_id, 
-                            speed_limit, vehicle_speed, license_plate, licenseplatereplydate, 
-                            licenseplatereplytime, vehicletype, vehiclestatuscode, photo, 
-                            recognize, license_plate2, location, address, longitude, latitude
-                            FROM trafficviolation WHERE violation_id = %s
-                            '''
-            cursor.execute(select_sql, (violation_id,))
-            violation = cursor.fetchone()
-            cursor.execute(select_sql, (violation_id,))
-            violation = cursor.fetchone()
-
-            if violation:
-                # Insert the entry into the abandoned table
-                insert_sql =    '''
-                                INSERT INTO abandoned (violation_id, violation_date, violation_time, device_id, 
-                                speed_limit, vehicle_speed, license_plate, licenseplatereplydate, 
-                                licenseplatereplytime, vehicletype, vehiclestatuscode, photo, 
-                                recognize, license_plate2, location, address, longitude, latitude)
-                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-                                '''
-                
-                cursor.execute(insert_sql, violation) 
-
-                # Delete the entry from the original table
-                delete_sql = '''DELETE FROM trafficviolation WHERE violation_id = %s'''
-                cursor.execute(delete_sql, (violation_id,))
-
-                print("License plate moved and deleted successfully.")
-            else:
-                print("Violation ID not found.")
+            # Use parameterized query to safely insert the name
+            sql = '''  
+                    UPDATE traffic_violation 
+                    SET status_code = %s
+                    WHERE violation_id = %s;
+                    '''
+            
+            # Execute the query with the 'name' argument passed as a parameter
+            cursor.execute(sql, (status_code, violation_id,))
+    
+    return ("Traffic violation status updated successfully.")
 
 @router.get("/violation/get_all")
 def get_all_issuable_violations():
@@ -136,7 +152,7 @@ def get_all_issuable_violations():
         with conn.cursor() as cursor:
 
             # Use parameterized query to safely insert the name
-            sql = '''SELECT * FROM trafficviolation where recognize = 0''' # 0 means recognized by AI and human
+            sql = '''SELECT * FROM traffic_violation where status_code = 0''' # 0 means recognized by AI and human
 
             # Execute the query with the 'name' argument passed as a parameter
             cursor.execute(sql)
@@ -146,24 +162,23 @@ def get_all_issuable_violations():
             
             for entry in fetched_entries:
                 # Convert the bytea image to base64
-                image_data = entry[11]  
+                image_data = entry[10]  
                 base64_image = base64.b64encode(image_data).decode('utf-8')
                 
                 entry = list(entry)
-                entry[11] = base64_image
+                entry[10] = base64_image
                 entries.append(tuple(entry))
                 
     return entries
 
-# print(get_all_issuable_violations())
 
 @router.get("/vehicle/get")
-def get_vehicle(plate_number):
+def get_vehicle_details(plate_number):
     with psycopg.connect(conninfo,autocommit=True) as conn:
         with conn.cursor() as cursor:
     
             # Use parameterized query to safely insert the name
-            sql = '''SELECT * FROM vehicles WHERE plateNumber = %s'''
+            sql = '''SELECT * FROM vehicle_registration WHERE license_plate = %s'''
             
             # Execute the query with the 'name' argument passed as a parameter
             cursor.execute(sql, (plate_number,))
@@ -179,26 +194,9 @@ def update_traffic_violation(violation_id):
         with conn.cursor() as cursor:
 
             # Use parameterized query to safely insert the name
-            sql = '''UPDATE trafficviolation SET recognize = 3 WHERE violation_id = %s'''
+            sql = '''UPDATE traffic_violation SET status_code = 30 WHERE violation_id = %s'''
             
             # Execute the query with the 'name' argument passed as a parameter
             cursor.execute(sql, (violation_id,))
     
-    print("Traffic violation updated successfully.")
-
-@router.post("/delete_ticket")
-def delete_ticket(license_plate):
-    with psycopg.connect(conninfo,autocommit=True) as conn:
-        with conn.cursor() as cursor:
-            # Use parameterized query to safely insert the name
-            sql = '''DELETE * FROM tickets WHERE license_plate = %s'''
-            cursor.execute(sql, license_plate)
-
-    print("Delete license plate successfully.")
-
-
-# get_license_plate()
-# print(get_driver())
-# get_all_violation()
-# print(get_all_driver())
-# print(get_all_ticket())
+    return ("Traffic violation issued successfully.")
